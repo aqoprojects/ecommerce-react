059d6d7a03a62d096308bea028026160cf0bc7a0
ajao quyum <ajaoquyumolamide@gmail.com>
2025-06-20 20:54:02 +0100

    Use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 import './HomePage.css';
@@ -5,14 +6,12 @@ import './HomePage.css';
 
 export function HomePage() {
 
-  fetch("http://localhost:3000/api/products")
-  .then((response)=>{
-    return response.json();
-  })
+  axios.get("http://localhost:3000/api/products")
+    .then((response)=>{
+     console.log(response.data);
+    })
 
-  .then((data)=>{
-        console.log(data);
-      })
+    
   return (
     <>
 
