eb57c2e36be4d0ccea1e3341fa77b00bcc7caa5e
ajao quyum <ajaoquyumolamide@gmail.com>
2025-06-20 21:20:51 +0100

    Add server proxy config for /api and /images

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -12,13 +12,13 @@ export function HomePage ()
 
   useEffect( () =>
   {
-    axios.get( "http://localhost:3000/api/products" )
+    axios.get( "/api/products" )
       .then( ( response ) =>
       {
         setProducts(response.data)
       } );
 
-      axios.get('http://localhost:3000/api/cart-items')
+      axios.get('/api/cart-items')
       .then((response)=>{
         setCart(response.data)
       })
