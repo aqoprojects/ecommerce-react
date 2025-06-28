37fb9258d09983169990a07a51e7df50797ecf69
ajao quyum <ajaoquyumolamide@gmail.com>
2025-06-28 17:36:52 +0100

    Use before each to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,28 +1,36 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { Product } from './Product';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 
-vi.mock('axios');
+vi.mock( 'axios' );
 
 describe( 'Product component', () =>
 {
+  let product;
+
+  let loadCart;
+
+  beforeEach(()=>{
+    product = {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: [ "socks", "sports", "apparel" ]
+  };
+
+  loadCart = vi.fn();
+  })
+
   it( 'displays the product details correctly', () =>
   {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87
-      },
-      priceCents: 1090,
-      keywords: [ "socks", "sports", "apparel" ]
-    };
 
-    const loadCart = vi.fn();
 
     render( <Product product={product} loadCart={loadCart} /> );
 
@@ -53,28 +61,14 @@ describe( 'Product component', () =>
   it( 'adds a product to the cart', async () =>
   {
 
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87
-      },
-      priceCents: 1090,
-      keywords: [ "socks", "sports", "apparel" ]
-    };
-
-    const loadCart = vi.fn();
-
     render( <Product product={product} loadCart={loadCart} /> );
 
     const user = userEvent.setup();
-    const addToCarrtButton = screen.getByTestId('add-to-car-button')
-    await user.click(addToCarrtButton )
+    const addToCarrtButton = screen.getByTestId( 'add-to-car-button' );
+    await user.click( addToCarrtButton );
 
 
-    expect(axios.post).toHaveBeenCalledWith(
+    expect( axios.post ).toHaveBeenCalledWith(
       '/api/cart-items',
       {
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -82,6 +76,6 @@ describe( 'Product component', () =>
       }
     );
 
-    expect(loadCart).toHaveBeenCalled()
+    expect( loadCart ).toHaveBeenCalled();
   } );
 } );
